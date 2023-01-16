3536e55b08a4e82bf8d87a0e4c41983b28602f30
shin.nervy.ta <nervy.toasts_0c@icloud.com>
2023-01-16 08:42:07 +0000

    再再提出

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -28,10 +28,10 @@ class TasksController < ApplicationController
     @task = Task.find(params[:id])
 
     if @task.update(task_params)
-      flash[:success] = 'Task は正常に更新されました'
+      flash[:success] = 'タスク は正常に更新されました'
       redirect_to @task
     else
-      flash.now[:danger] = 'Task は更新されませんでした'
+      flash.now[:danger] = 'タスク は更新されませんでした'
       render :edit
     end
   end
@@ -44,7 +44,7 @@ class TasksController < ApplicationController
     @task = Task.find(params[:id])
     @task.destroy
 
-    flash[:success] = 'Task は正常に削除されました'
+    flash[:success] = 'タスク は正常に削除されました'
     redirect_to tasks_url
   end
 
