3db106af7d8e066c7f3d77b9fd1e69f1a699c944
shin.nervy.ta <nervy.toasts_0c@icloud.com>
2023-01-20 06:27:01 +0000

    再度提出

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_01_15_084935) do
+ActiveRecord::Schema.define(version: 2023_01_19_140741) do
 
   create_table "tasks", charset: "utf8mb4", force: :cascade do |t|
     t.string "content"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "status"
   end
 
 end
